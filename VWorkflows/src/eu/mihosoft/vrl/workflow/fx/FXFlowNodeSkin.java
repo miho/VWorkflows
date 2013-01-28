5f5a762da28f71775ce77877651ad23d814ec92b
Michael Hoffer <info@michaelhoffer.de>
2013-01-28 17:00:43 +0100

    preparing jdk 8 support

--- VWorkflows/src/eu/mihosoft/vrl/workflow/fx/FXFlowNodeSkin.java
+++ VWorkflows/src/eu/mihosoft/vrl/workflow/fx/FXFlowNodeSkin.java
@@ -176,52 +176,52 @@ public class FXFlowNodeSkin
         });
     }
 
-    private void delegateEventsTo(Node src, final Node target) {
-        // link: https://forums.oracle.com/forums/thread.jspa?threadID=2425879
-        EventHandler<MouseEvent> interceptHandler = new EventHandler<MouseEvent>() {
-            //is the fixed copy fired already - controls the recursion
-            boolean isEventCopyFired = false;
+//    private void delegateEventsTo(Node src, final Node target) {
+//        // link: https://forums.oracle.com/forums/thread.jspa?threadID=2425879
+//        EventHandler<MouseEvent> interceptHandler = new EventHandler<MouseEvent>() {
+//            //is the fixed copy fired already - controls the recursion
+//            boolean isEventCopyFired = false;
+//
+//            @Override
+//            public void handle(MouseEvent e) {
+//                try {
+//                    if (!isEventCopyFired) {
+//                        isEventCopyFired = true; //prevents further recursion
+//                        //copy the original event but set the appropriate isShiftDown and isControlDown flags
+//                        MouseEvent copy = copyEvent(e);
+//
+//                        MouseEvent.fireEvent(target, copy); //this causes a recursion 
+//
+//                        e.consume(); //consume the original event, because we fired a copy
+//
+//                        //turn off the flag for the next event
+//                        isEventCopyFired = false;
+//                    }
+//                } catch (Exception ex) {//impl_ methods can be removed in the future, ensure we don't break event handling 
+//                    ex.printStackTrace(System.err);
+//                }
+//            }
+//        };
+//        //mouse pressed events deal with selection of items
+//        src.addEventFilter(MouseEvent.MOUSE_PRESSED, interceptHandler);
+//        src.addEventFilter(MouseEvent.MOUSE_DRAGGED, interceptHandler);
+//    }
 
-            @Override
-            public void handle(MouseEvent e) {
-                try {
-                    if (!isEventCopyFired) {
-                        isEventCopyFired = true; //prevents further recursion
-                        //copy the original event but set the appropriate isShiftDown and isControlDown flags
-                        MouseEvent copy = copyEvent(e);
-
-                        MouseEvent.fireEvent(target, copy); //this causes a recursion 
-
-                        e.consume(); //consume the original event, because we fired a copy
-
-                        //turn off the flag for the next event
-                        isEventCopyFired = false;
-                    }
-                } catch (Exception ex) {//impl_ methods can be removed in the future, ensure we don't break event handling 
-                    ex.printStackTrace(System.err);
-                }
-            }
-        };
-        //mouse pressed events deal with selection of items
-        src.addEventFilter(MouseEvent.MOUSE_PRESSED, interceptHandler);
-        src.addEventFilter(MouseEvent.MOUSE_DRAGGED, interceptHandler);
-    }
-
-    private MouseEvent copyEvent(MouseEvent e) {
-        MouseEvent copy = MouseEvent.impl_mouseEvent(
-                e.getSceneX(), e.getSceneY(),
-                e.getScreenX(), e.getScreenY(),
-                e.getButton(), e.getClickCount(),
-                e.isShiftDown(), e.isControlDown(),
-                e.isAltDown(), e.isMetaDown(),
-                MouseEvent.impl_getPopupTrigger(e),
-                e.isPrimaryButtonDown(),
-                e.isMiddleButtonDown(),
-                e.isSecondaryButtonDown(),
-                e.isSynthesized(),
-                (EventType<MouseEvent>) e.getEventType());
-        return copy;
-    }
+//    private MouseEvent copyEvent(MouseEvent e) {
+//        MouseEvent copy = MouseEvent.impl_mouseEvent(
+//                e.getSceneX(), e.getSceneY(),
+//                e.getScreenX(), e.getScreenY(),
+//                e.getButton(), e.getClickCount(),
+//                e.isShiftDown(), e.isControlDown(),
+//                e.isAltDown(), e.isMetaDown(),
+//                MouseEvent.impl_getPopupTrigger(e),
+//                e.isPrimaryButtonDown(),
+//                e.isMiddleButtonDown(),
+//                e.isSecondaryButtonDown(),
+//                e.isSynthesized(),
+//                (EventType<MouseEvent>) e.getEventType());
+//        return copy;
+//    }
 
     private void removeOutputConnector() {
         if (output != null) {

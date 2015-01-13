189af6578f2c49e9aa3c2e87ea4ed85920a8e332
Michael Hoffer <info@michaelhoffer.de>
2015-01-13 17:53:34 +0100

    connectionclass test added

---
+++ VWorkflows-Core/src/test/java/eu/mihosoft/vrl/workflow/ConnectionImplClassTest.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eu.mihosoft.vrl.workflow;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ *
+ * @author Michael Hoffer &lt;info@michaelhoffer.de&gt;
+ */
+public class ConnectionImplClassTest {
+
+    @Test
+    public void testImplClassWithConformingConstructor() {
+        VFlow flow = FlowFactory.newFlow();
+        
+        flow.getConnections("mytype").setConnectionClass(ConnectionBase.class);
+    }
+
+    public void testImplClassWithNonConformingConstructor() {
+        VFlow flow = FlowFactory.newFlow();
+        
+         boolean exceptionThrown = false;
+        
+        try{
+        flow.getConnections("mytype").setConnectionClass(
+                ConnectionBaseWithoutConformingConstructor.class);
+        } catch(IllegalArgumentException ex) {
+            exceptionThrown = true;
+        }
+        
+        Assert.assertTrue("IllegalArgumentException must be thrown!", exceptionThrown);
+    }
+}
+
+class ConnectionBaseWithoutConformingConstructor extends ConnectionBase {
+    //
+    private ConnectionBaseWithoutConformingConstructor() {};
+}

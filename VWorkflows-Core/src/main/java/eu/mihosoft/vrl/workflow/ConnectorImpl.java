39f1c8e89d0e13cb573b3de3c6566a3644373e0c
aalmiray <aalmiray@gmail.com>
2016-01-22 20:30:14 +0100

    Update license header; apply license header to all files

--- VWorkflows-Core/src/main/java/eu/mihosoft/vrl/workflow/ConnectorImpl.java
+++ VWorkflows-Core/src/main/java/eu/mihosoft/vrl/workflow/ConnectorImpl.java
@@ -1,7 +1,5 @@
 /*
- * ConnectorImpl.java
- * 
- * Copyright 2012-2013 Michael Hoffer <info@michaelhoffer.de>. All rights reserved.
+ * Copyright 2012-2016 Michael Hoffer <info@michaelhoffer.de>. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification, are
  * permitted provided that the following conditions are met:
@@ -12,7 +10,7 @@
  *    2. Redistributions in binary form must reproduce the above copyright notice, this list
  *       of conditions and the following disclaimer in the documentation and/or other materials
  *       provided with the distribution.
- * 
+ *
  * Please cite the following publication(s):
  *
  * M. Hoffer, C.Poliwoda, G.Wittum. Visual Reflection Library -
--- VWorkflows-FX/src/main/java/eu/mihosoft/vrl/workflow/fx/WindowUtil.java
+++ VWorkflows-FX/src/main/java/eu/mihosoft/vrl/workflow/fx/WindowUtil.java
@@ -1,7 +1,5 @@
 /*
- * WindowUtil.java
- * 
- * Copyright 2012-2013 Michael Hoffer <info@michaelhoffer.de>. All rights reserved.
+ * Copyright 2012-2016 Michael Hoffer <info@michaelhoffer.de>. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification, are
  * permitted provided that the following conditions are met:
@@ -12,7 +10,7 @@
  *    2. Redistributions in binary form must reproduce the above copyright notice, this list
  *       of conditions and the following disclaimer in the documentation and/or other materials
  *       provided with the distribution.
- * 
+ *
  * Please cite the following publication(s):
  *
  * M. Hoffer, C.Poliwoda, G.Wittum. Visual Reflection Library -
@@ -32,8 +30,7 @@
  * The views and conclusions contained in the software and documentation are those of the
  * authors and should not be interpreted as representing official policies, either expressed
  * or implied, of Michael Hoffer <info@michaelhoffer.de>.
- */ 
-
+ */
 package eu.mihosoft.vrl.workflow.fx;
 
 import jfxtras.labs.scene.control.window.CloseIcon;

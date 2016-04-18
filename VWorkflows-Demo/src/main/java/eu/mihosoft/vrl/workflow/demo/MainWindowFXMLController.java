a2662f3e2928c7f6fe129ab1a97114e0b51cd08d
Michael Hoffer <info@michaelhoffer.de>
2016-04-18 19:00:52 +0200

    bintray publication should work now

--- VWorkflows-Demo/src/main/java/eu/mihosoft/vrl/workflow/demo/MainWindowFXMLController.java
+++ VWorkflows-Demo/src/main/java/eu/mihosoft/vrl/workflow/demo/MainWindowFXMLController.java
@@ -53,14 +53,13 @@ import javafx.scene.control.ContextMenu;
 import javafx.scene.control.MenuItem;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
-import jfxtras.labs.scene.control.window.Window;
-
 import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Paths;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import jfxtras.scene.control.window.Window;
 
 /**
  * FXML Controller class

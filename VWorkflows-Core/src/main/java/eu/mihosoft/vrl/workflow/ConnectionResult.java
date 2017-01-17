afa45119bee6904665b11fa837ded54e015166de
aalmiray <aalmiray@gmail.com>
2017-01-17 15:53:21 +0100

    Update license headers

--- VWorkflows-Core/src/main/java/eu/mihosoft/vrl/workflow/ConnectionResult.java
+++ VWorkflows-Core/src/main/java/eu/mihosoft/vrl/workflow/ConnectionResult.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2016 Michael Hoffer <info@michaelhoffer.de>. All rights reserved.
+ * Copyright 2012-2017 Michael Hoffer <info@michaelhoffer.de>. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification, are
  * permitted provided that the following conditions are met:
--- VWorkflows-FX/src/test/java/DummyTest.java
+++ VWorkflows-FX/src/test/java/DummyTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2016 Michael Hoffer <info@michaelhoffer.de>. All rights reserved.
+ * Copyright 2012-2017 Michael Hoffer <info@michaelhoffer.de>. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification, are
  * permitted provided that the following conditions are met:
@@ -31,7 +31,6 @@
  * authors and should not be interpreted as representing official policies, either expressed
  * or implied, of Michael Hoffer <info@michaelhoffer.de>.
  */
-
 import org.junit.Test;
 
 import static org.junit.Assert.assertTrue;
